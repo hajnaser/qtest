875659fc06f9648816adbc381127b6a9693cf213
haj.naser <m@nvser.ir>
2016-09-15 02:05:17 +0430

    Update Luncher.php

--- Luncher.php
+++ Luncher.php
@@ -1,5 +1,5 @@
 <?php
-define('BOT_TOKEN', '**TOKEN**');
+define('BOT_TOKEN', '295945927:AAHHPqWViGoD1TVSk_ttusMSciciJ73vkng');
 define('API_URL', 'https://api.telegram.org/bot'.BOT_TOKEN.'/');
 
 function apiRequestWebhook($method, $parameters) {
